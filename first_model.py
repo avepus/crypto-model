449bbff2643edcef9d3be2e9dc6793c80830e387
Jakefrda <54994150+Jakefrda@users.noreply.github.com>
2020-12-06 16:07:53 -0800

    Began updating first_model.py
    Began importing necessary packages, creating basic visualization of data, setting up basic structure.  Referenced websites for modeling ideas added in comments.

--- first_model.py
+++ first_model.py
@@ -1,7 +1,62 @@
 """
-Created on Sat Apr 25 22:09:42 2020
+Created on Dec 6th
 
 @author: Avery And Jake
+
+Website References:
+different model options - https://www.kaggle.com/kashnitsky/topic-9-part-1-time-series-analysis-in-python
+forecasting bitcoin with python - https://www.kaggle.com/vennaa/notebook-forecasting-bitcoins
+
 """
 
-print("Helloworld")
+
+
+# Import Data
+import warnings  # do not disturb mode
+import sklearn
+
+warnings.filterwarnings('ignore')
+
+import numpy as np  # vectors and matrices
+import pandas as pd  # tables and data manipulation
+import matplotlib.pyplot as plt  # plots
+from sklearn.metrics import r2_score, median_absolute_error, mean_absolute_error
+from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squared_log_error
+
+
+# Global presets
+csv_source = 'ohlcv_data\MarketDataVIBBTC.csv'
+
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    '''
+        MAPE for error analysis
+    '''
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def main():
+    '''
+    Returns - Currently just data exploration
+    -------
+    '''
+
+    df = pd.read_csv(csv_source, index_col=['Time_Converted'],parse_dates=['Time_Converted']) # Time_Converted is a column that was manually added to the dataset with the formula =(A2/86400000)+25569 to convert to excel date. Add code to complete automatically
+
+
+    print(df.head())
+    plt.figure(figsize=(15, 7))
+    plt.plot(df.Open)
+    plt.plot(df.Close)
+    plt.title('Historicalical Open/Close')
+    plt.grid(True)
+    plt.show()
+
+    plt.figure(figsize=(15, 7))
+    plt.plot(df.Volume)
+    plt.title('Historicalical Volume')
+    plt.grid(True)
+    plt.show()
+
+main()
