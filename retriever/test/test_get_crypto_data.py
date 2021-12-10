455133d565c2b29ca3fbd9df40be4d42bc3668b1
avepus <avepus@gmail.com>
2021-12-09 20:33:10 -0600

    test updates

--- retriever/test/test_get_crypto_data.py
+++ retriever/test/test_get_crypto_data.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import pandas as pd
 from rba_tools.retriever.timeframe import Timeframe
 from datetime import datetime
@@ -9,7 +10,8 @@ import rba_tools.retriever.database_interface as dbi
 from dateutil import parser
 from pathlib import Path
 
-PERFORM_API_TESTS = True
+#would be best to make this use "mock" to get api values without calling
+PERFORM_API_TESTS = False
 
 
 
@@ -20,13 +22,17 @@ class TestMain(unittest.TestCase):
         db = dbi.SQLite3OHLCVDatabase(test=True)
         if os.path.exists(db.get_database_file()):
             os.remove(db.get_database_file())
+
+        self.sqlite_database = dbi.SQLite3OHLCVDatabase(test=True)
+        self.sqlite_retriever = retrievers.DatabaseRetriever(self.sqlite_database)
+        self.ccxt_retriever = retrievers.CCXTDataRetriever('binance')
+        file = str(Path(__file__).parent) + '\ETH_BTC_1H_2020-1-1.csv'
+        self.csv_retriver = retrievers.CSVDataRetriever(file)
     
-    def test_main_online_then_stored(self):
-        """calls main method with some defaults"""
-        database = dbi.SQLite3OHLCVDatabase(test=True)
-        stored_retriever = retrievers.DatabaseRetriever(database)
-        online_retriever = retrievers.CCXTDataRetriever('binance')
-        puller = gcd.DataPuller(stored_retriever, online_retriever, database)
+    def test_main_online(self):
+        if not PERFORM_API_TESTS:
+            return
+        puller = gcd.DataPuller(online_retriever=self.ccxt_retriever)
 
         symbol = 'ETH/BTC'
         timeframe_str = '1h'
@@ -40,12 +46,26 @@ class TestMain(unittest.TestCase):
 
         pd.testing.assert_frame_equal(expected, result)
 
-    
 
-    
+    def test_main_retrive_and_stored(self):
+        csv_puller = gcd.DataPuller(online_retriever=self.csv_retriver, database=self.sqlite_database)
+        stored_puller = gcd.DataPuller(stored_retriever=self.sqlite_retriever, database=self.sqlite_database)
 
-    def test_main_stored_only(self):
-        pass
+        symbol = 'ETH/BTC'
+        timeframe_str = '1h'
+        from_date_str = '12-1-2020'
+        to_date_str = '12-20-2020'
+
+        with patch('rba_tools.retriever.get_crypto_data.retrievers.CSVDataRetriever.fetch_ohlcv', return_value=gcd.get_empty_ohlcv_df()):
+            csv_result = csv_puller.fetch_df(symbol, timeframe_str, from_date_str, to_date_str)
+        stored_result = stored_puller.fetch_df(symbol, timeframe_str, from_date_str, to_date_str)
+
+
+        file = str(Path(__file__).parent) + '\ETH_BTC_1H_2020-1-1.csv'
+        expected = pd.read_csv(file, parse_dates=True, index_col='Timestamp')
+
+        pd.testing.assert_frame_equal(expected, csv_result)
+        pd.testing.assert_frame_equal(stored_result, csv_result)
 
     def test_main_online_only(self):
         pass
