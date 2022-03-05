9cee35d54b7d7701be72b1d5e78b22c4fa56544b
avepus <avepus@gmail.com>
2022-03-04 20:31:37 -0600

    got some plot stuff going

--- src/rba_tools/backtest/backtrader_extensions/plot.py
+++ src/rba_tools/backtest/backtrader_extensions/plot.py
@@ -1,6 +1,541 @@
-#my modified code for reference
+import backtrader as bt
+from datetime import datetime
+import plotly.graph_objects as go
+from backtrader.utils import num2date
+import pandas as pd
+import numpy as np
+
+def get_datetime(strategy):
+    datetime_series = pd.Series(strategy.datetime.plot())
+    datetime_array = datetime_series.map(num2date)
+    return pd.to_datetime(datetime_array)
 
 
+#my attempt
+def my_plot(self, strategy, plotter=None, start=None, end=None, **kwargs):
+    if self._exactbars > 0:
+        return
+
+    #do the plotting
+
+
+def plot(self, strategy, figid=0, numfigs=1, iplot=True, **kwargs):
+        # pfillers={}):
+        if not strategy.datas:
+            return
+
+        if not len(strategy):
+            return
+
+        self.sortdataindicators(strategy)
+        self.calcrows(strategy)
+
+        st_dtime = strategy.lines.datetime.plot()
+        start = 0
+        end = len(st_dtime)
+
+        # slen = len(st_dtime[start:end])
+        # d, m = divmod(slen, numfigs)
+        # pranges = list()
+        # for i in range(numfigs):
+        #     a = d * i + start
+        #     if i == (numfigs - 1):
+        #         d += m  # add remainder to last stint
+        #     b = a + d
+
+        #     pranges.append([a, b, d])
+
+        figs = []
+
+        # prepare a figure
+        fig = self.pinf.newfig(figid, numfig, self.mpyplot)
+        figs.append(fig)
+
+        self.pinf.pstart, self.pinf.pend, self.pinf.psize = pranges[numfig]
+        self.pinf.xstart = self.pinf.pstart
+        self.pinf.xend = self.pinf.pend
+
+        self.pinf.clock = strategy
+        self.pinf.xreal = self.pinf.clock.datetime.plot(
+            self.pinf.pstart, self.pinf.psize)
+        self.pinf.xlen = len(self.pinf.xreal)
+        self.pinf.x = list(range(self.pinf.xlen))
+        # self.pinf.pfillers = {None: []}
+        # for key, val in pfillers.items():
+        #     pfstart = bisect.bisect_left(val, self.pinf.pstart)
+        #     pfend = bisect.bisect_right(val, self.pinf.pend)
+        #     self.pinf.pfillers[key] = val[pfstart:pfend]
+
+        # Do the plotting
+        # Things that go always at the top (observers)
+        for ptop in self.dplotstop:
+            self.plotind(None, ptop, subinds=self.dplotsover[ptop])
+
+        # Create the rest on a per data basis
+        for data in strategy.datas:
+            if not data.plotinfo.plot:
+                continue
+
+            self.pinf.xdata = self.pinf.x
+            xd = data.datetime.plotrange(self.pinf.xstart, self.pinf.xend)
+            if len(xd) < self.pinf.xlen:
+                self.pinf.xdata = xdata = []
+                xreal = self.pinf.xreal
+                dts = data.datetime.plot()
+                xtemp = list()
+                for dt in (x for x in dts if dt0 <= x <= dt1):
+                    dtidx = bisect.bisect_left(xreal, dt)
+                    xdata.append(dtidx)
+                    xtemp.append(dt)
+
+                self.pinf.xstart = bisect.bisect_left(dts, xtemp[0])
+                self.pinf.xend = bisect.bisect_right(dts, xtemp[-1])
+
+            for ind in self.dplotsup[data]:
+                self.plotind(
+                    data,
+                    ind,
+                    subinds=self.dplotsover[ind],
+                    upinds=self.dplotsup[ind],
+                    downinds=self.dplotsdown[ind])
+
+            self.plotdata(data, self.dplotsover[data])
+
+            for ind in self.dplotsdown[data]:
+                self.plotind(
+                    data,
+                    ind,
+                    subinds=self.dplotsover[ind],
+                    upinds=self.dplotsup[ind],
+                    downinds=self.dplotsdown[ind])
+
+
+        return figs
+
+def testplotind(indicator: bt.indicator, x_axis):
+    for lineidx in range(indicator.size()):
+            line = indicator.lines[lineidx]
+            linealias = indicator.lines._getlinealias(lineidx)
+
+            lineplotinfo = getattr(indicator.plotlines, '_%d' % lineidx, None)
+            if not lineplotinfo:
+                lineplotinfo = getattr(indicator.plotlines, linealias, None)
+
+            if not lineplotinfo:
+                lineplotinfo = bt.AutoInfoClass()
+
+            if lineplotinfo._get('_plotskip', False):
+                continue
+
+
+            # plot data
+            indicator_vals = line.plotrange(0, len(line))
+
+            return go.Scatter(x=x_axis,y=np.array(indicator_vals))
+
+
+
+def plotind(self, iref, ind,
+                subinds=None, upinds=None, downinds=None,
+                masterax=None):
+
+        sch = self.p.scheme
+
+        # check subind
+        subinds = subinds or []
+        upinds = upinds or []
+        downinds = downinds or []
+
+        # plot subindicators on self with independent axis above
+        for upind in upinds:
+            self.plotind(iref, upind)
+
+        # Get an axis for this plot
+        ax = masterax or self.newaxis(ind, rowspan=self.pinf.sch.rowsminor)
+
+        indlabel = ind.plotlabel()
+
+        for lineidx in range(ind.size()):
+            line = ind.lines[lineidx]
+            linealias = ind.lines._getlinealias(lineidx)
+
+            lineplotinfo = getattr(ind.plotlines, '_%d' % lineidx, None)
+            if not lineplotinfo:
+                lineplotinfo = getattr(ind.plotlines, linealias, None)
+
+            if not lineplotinfo:
+                lineplotinfo = bt.AutoInfoClass()
+
+            if lineplotinfo._get('_plotskip', False):
+                continue
+
+
+            # plot data
+            lplot = line.plotrange(self.pinf.xstart, self.pinf.xend)
+
+            # Global and generic for indicator
+            if self.pinf.sch.linevalues and ind.plotinfo.plotlinevalues:
+                plotlinevalue = lineplotinfo._get('_plotvalue', True)
+                if plotlinevalue and not math.isnan(lplot[-1]):
+                    label += ' %.2f' % lplot[-1]
+
+            plotkwargs = dict()
+            linekwargs = lineplotinfo._getkwargs(skip_=True)
+
+            if linekwargs.get('color', None) is None:
+                if not lineplotinfo._get('_samecolor', False):
+                    self.pinf.nextcolor(ax)
+                plotkwargs['color'] = self.pinf.color(ax)
+
+            plotkwargs.update(dict(aa=True, label=label))
+            plotkwargs.update(**linekwargs)
+
+            if ax in self.pinf.zorder:
+                plotkwargs['zorder'] = self.pinf.zordernext(ax)
+
+            pltmethod = getattr(ax, lineplotinfo._get('_method', 'plot'))
+
+            xdata, lplotarray = self.pinf.xdata, lplot
+            if lineplotinfo._get('_skipnan', False):
+                # Get the full array and a mask to skipnan
+                lplotarray = np.array(lplot)
+                lplotmask = np.isfinite(lplotarray)
+
+                # Get both the axis and the data masked
+                lplotarray = lplotarray[lplotmask]
+                xdata = np.array(xdata)[lplotmask]
+
+            plottedline = pltmethod(xdata, lplotarray, **plotkwargs)
+            try:
+                plottedline = plottedline[0]
+            except:
+                # Possibly a container of artists (when plotting bars)
+                pass
+
+            self.pinf.zorder[ax] = plottedline.get_zorder()
+
+            vtags = lineplotinfo._get('plotvaluetags', True)
+            if self.pinf.sch.valuetags and vtags:
+                linetag = lineplotinfo._get('_plotvaluetag', True)
+                if linetag and not math.isnan(lplot[-1]):
+                    # line has valid values, plot a tag for the last value
+                    self.drawtag(ax, len(self.pinf.xreal), lplot[-1],
+                                 facecolor='white',
+                                 edgecolor=self.pinf.color(ax))
+
+            farts = (('_gt', operator.gt), ('_lt', operator.lt), ('', None),)
+            for fcmp, fop in farts:
+                fattr = '_fill' + fcmp
+                fref, fcol = lineplotinfo._get(fattr, (None, None))
+                if fref is not None:
+                    y1 = np.array(lplot)
+                    if isinstance(fref, integer_types):
+                        y2 = np.full_like(y1, fref)
+                    else:  # string, naming a line, nothing else is supported
+                        l2 = getattr(ind, fref)
+                        prl2 = l2.plotrange(self.pinf.xstart, self.pinf.xend)
+                        y2 = np.array(prl2)
+                    kwargs = dict()
+                    if fop is not None:
+                        kwargs['where'] = fop(y1, y2)
+
+                    falpha = self.pinf.sch.fillalpha
+                    if isinstance(fcol, (list, tuple)):
+                        fcol, falpha = fcol
+
+                    ax.fill_between(self.pinf.xdata, y1, y2,
+                                    facecolor=fcol,
+                                    alpha=falpha,
+                                    interpolate=True,
+                                    **kwargs)
+
+        # plot subindicators that were created on self
+        for subind in subinds:
+            self.plotind(iref, subind, subinds=self.dplotsover[subind],
+                         masterax=ax)
+
+        if not masterax:
+            # adjust margin if requested ... general of particular
+            ymargin = ind.plotinfo._get('plotymargin', 0.0)
+            ymargin = max(ymargin, self.pinf.sch.yadjust)
+            if ymargin:
+                ax.margins(y=ymargin)
+
+            # Set specific or generic ticks
+            yticks = ind.plotinfo._get('plotyticks', [])
+            if not yticks:
+                yticks = ind.plotinfo._get('plotyhlines', [])
+
+            if yticks:
+                ax.set_yticks(yticks)
+            else:
+                locator = mticker.MaxNLocator(nbins=4, prune='both')
+                ax.yaxis.set_major_locator(locator)
+
+            # Set specific hlines if asked to
+            hlines = ind.plotinfo._get('plothlines', [])
+            if not hlines:
+                hlines = ind.plotinfo._get('plotyhlines', [])
+            for hline in hlines:
+                ax.axhline(hline, color=self.pinf.sch.hlinescolor,
+                           ls=self.pinf.sch.hlinesstyle,
+                           lw=self.pinf.sch.hlineswidth)
+
+            if self.pinf.sch.legendind and \
+               ind.plotinfo._get('plotlegend', True):
+
+                handles, labels = ax.get_legend_handles_labels()
+                # Ensure that we have something to show
+                if labels:
+                    # location can come from the user
+                    loc = ind.plotinfo.legendloc or self.pinf.sch.legendindloc
+
+                    # Legend done here to ensure it includes all plots
+                    legend = ax.legend(loc=loc,
+                                       numpoints=1, frameon=False,
+                                       shadow=False, fancybox=False,
+                                       prop=self.pinf.prop)
+
+                    # legend.set_title(indlabel, prop=self.pinf.prop)
+                    # hack: if title is set. legend has a Vbox for the labels
+                    # which has a default "center" set
+                    legend._legend_box.align = 'left'
+
+        # plot subindicators on self with independent axis below
+        for downind in downinds:
+            self.plotind(iref, downind)
+
+
+
+
+
+
+
+
+
+
+
+
+#code from backtrader.cerebro.plot is below
+def plot(self, plotter=None, numfigs=1, iplot=True, start=None, end=None,
+             width=16, height=9, dpi=300, tight=True, use=None,
+             **kwargs):
+        '''
+        Plots the strategies inside cerebro
+
+        If ``plotter`` is None a default ``Plot`` instance is created and
+        ``kwargs`` are passed to it during instantiation.
+
+        ``numfigs`` split the plot in the indicated number of charts reducing
+        chart density if wished
+
+        ``iplot``: if ``True`` and running in a ``notebook`` the charts will be
+        displayed inline
+
+        ``use``: set it to the name of the desired matplotlib backend. It will
+        take precedence over ``iplot``
+
+        ``start``: An index to the datetime line array of the strategy or a
+        ``datetime.date``, ``datetime.datetime`` instance indicating the start
+        of the plot
+
+        ``end``: An index to the datetime line array of the strategy or a
+        ``datetime.date``, ``datetime.datetime`` instance indicating the end
+        of the plot
+
+        ``width``: in inches of the saved figure
+
+        ``height``: in inches of the saved figure
+
+        ``dpi``: quality in dots per inches of the saved figure
+
+        ``tight``: only save actual content and not the frame of the figure
+        '''
+        if self._exactbars > 0:
+            return
+
+        if not plotter:
+            from backtrader import plot
+            if self.p.oldsync:
+                plotter = plot.Plot_OldSync(**kwargs)
+            else:
+                plotter = plot.Plot(**kwargs)
+
+        # pfillers = {self.datas[i]: self._plotfillers[i]
+        # for i, x in enumerate(self._plotfillers)}
+
+        # pfillers2 = {self.datas[i]: self._plotfillers2[i]
+        # for i, x in enumerate(self._plotfillers2)}
+
+        figs = []
+        for stratlist in self.runstrats:
+            for si, strat in enumerate(stratlist):
+                rfig = plotter.plot(strat, figid=si * 100,
+                                    numfigs=numfigs, iplot=iplot,
+                                    start=start, end=end, use=use)
+                # pfillers=pfillers2)
+
+                figs.append(rfig)
+
+            plotter.show()
+
+#code from backtrader.Plot.plot is below
+def plot(self, strategy, figid=0, numfigs=1, iplot=True,
+             start=None, end=None, **kwargs):
+        # pfillers={}):
+        if not strategy.datas:
+            return
+
+        if not len(strategy):
+            return
+
+        if iplot:
+            if 'ipykernel' in sys.modules:
+                matplotlib.use('nbagg')
+
+        # this import must not happen before matplotlib.use
+        import matplotlib.pyplot as mpyplot
+        self.mpyplot = mpyplot
+
+        self.pinf = PInfo(self.p.scheme)
+        self.sortdataindicators(strategy)
+        self.calcrows(strategy)
+
+        st_dtime = strategy.lines.datetime.plot()
+        if start is None:
+            start = 0
+        if end is None:
+            end = len(st_dtime)
+
+        if isinstance(start, datetime.date):
+            start = bisect.bisect_left(st_dtime, date2num(start))
+
+        if isinstance(end, datetime.date):
+            end = bisect.bisect_right(st_dtime, date2num(end))
+
+        if end < 0:
+            end = len(st_dtime) + 1 + end  # -1 =  len() -2 = len() - 1
+
+        slen = len(st_dtime[start:end])
+        d, m = divmod(slen, numfigs)
+        pranges = list()
+        for i in range(numfigs):
+            a = d * i + start
+            if i == (numfigs - 1):
+                d += m  # add remainder to last stint
+            b = a + d
+
+            pranges.append([a, b, d])
+
+        figs = []
+
+        for numfig in range(numfigs):
+            # prepare a figure
+            fig = self.pinf.newfig(figid, numfig, self.mpyplot)
+            figs.append(fig)
+
+            self.pinf.pstart, self.pinf.pend, self.pinf.psize = pranges[numfig]
+            self.pinf.xstart = self.pinf.pstart
+            self.pinf.xend = self.pinf.pend
+
+            self.pinf.clock = strategy
+            self.pinf.xreal = self.pinf.clock.datetime.plot(
+                self.pinf.pstart, self.pinf.psize)
+            self.pinf.xlen = len(self.pinf.xreal)
+            self.pinf.x = list(range(self.pinf.xlen))
+            # self.pinf.pfillers = {None: []}
+            # for key, val in pfillers.items():
+            #     pfstart = bisect.bisect_left(val, self.pinf.pstart)
+            #     pfend = bisect.bisect_right(val, self.pinf.pend)
+            #     self.pinf.pfillers[key] = val[pfstart:pfend]
+
+            # Do the plotting
+            # Things that go always at the top (observers)
+            self.pinf.xdata = self.pinf.x
+            for ptop in self.dplotstop:
+                self.plotind(None, ptop, subinds=self.dplotsover[ptop])
+
+            # Create the rest on a per data basis
+            dt0, dt1 = self.pinf.xreal[0], self.pinf.xreal[-1]
+            for data in strategy.datas:
+                if not data.plotinfo.plot:
+                    continue
+
+                self.pinf.xdata = self.pinf.x
+                xd = data.datetime.plotrange(self.pinf.xstart, self.pinf.xend)
+                if len(xd) < self.pinf.xlen:
+                    self.pinf.xdata = xdata = []
+                    xreal = self.pinf.xreal
+                    dts = data.datetime.plot()
+                    xtemp = list()
+                    for dt in (x for x in dts if dt0 <= x <= dt1):
+                        dtidx = bisect.bisect_left(xreal, dt)
+                        xdata.append(dtidx)
+                        xtemp.append(dt)
+
+                    self.pinf.xstart = bisect.bisect_left(dts, xtemp[0])
+                    self.pinf.xend = bisect.bisect_right(dts, xtemp[-1])
+
+                for ind in self.dplotsup[data]:
+                    self.plotind(
+                        data,
+                        ind,
+                        subinds=self.dplotsover[ind],
+                        upinds=self.dplotsup[ind],
+                        downinds=self.dplotsdown[ind])
+
+                self.plotdata(data, self.dplotsover[data])
+
+                for ind in self.dplotsdown[data]:
+                    self.plotind(
+                        data,
+                        ind,
+                        subinds=self.dplotsover[ind],
+                        upinds=self.dplotsup[ind],
+                        downinds=self.dplotsdown[ind])
+
+            cursor = MultiCursor(
+                fig.canvas, list(self.pinf.daxis.values()),
+                useblit=True,
+                horizOn=True, vertOn=True,
+                horizMulti=False, vertMulti=True,
+                horizShared=True, vertShared=False,
+                color='black', lw=1, ls=':')
+
+            self.pinf.cursors.append(cursor)
+
+            # Put the subplots as indicated by hspace
+            fig.subplots_adjust(hspace=self.pinf.sch.plotdist,
+                                top=0.98, left=0.05, bottom=0.05, right=0.95)
+
+            laxis = list(self.pinf.daxis.values())
+
+            # Find last axis which is not a twinx (date locator fails there)
+            i = -1
+            while True:
+                lastax = laxis[i]
+                if lastax not in self.pinf.vaxis:
+                    break
+
+                i -= 1
+
+            self.setlocators(lastax)  # place the locators/fmts
+
+            # Applying fig.autofmt_xdate if the data axis is the last one
+            # breaks the presentation of the date labels. why?
+            # Applying the manual rotation with setp cures the problem
+            # but the labels from all axis but the last have to be hidden
+            for ax in laxis:
+                self.mpyplot.setp(ax.get_xticklabels(), visible=False)
+
+            self.mpyplot.setp(lastax.get_xticklabels(), visible=True,
+                              rotation=self.pinf.sch.tickrotation)
+
+            # Things must be tight along the x axis (to fill both ends)
+            axtight = 'x' if not self.pinf.sch.ytight else 'both'
+            self.mpyplot.autoscale(enable=True, axis=axtight, tight=True)
+
+        return figs
 
 #code from backtrader.cerebro.plot is below
 def plot(self, plotter=None, numfigs=1, iplot=True, start=None, end=None,
